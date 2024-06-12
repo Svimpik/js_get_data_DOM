bf6bd86874d8130e60b6cec08aecc7194231dc35
Pavlo Svinarchuk <shokkontent2004@gmail.com>
2024-06-12 21:44:53 +0300

    make build

---
+++ index.f75de5e1.js
@@ -0,0 +1,13 @@
+"use strict";
+const parsePopulation = (text)=>Number(text.replace(/,/g, ""));
+const container = document.querySelector(".container");
+const arrayOfCountryPeople = [
+    ...container.querySelectorAll(".population")
+];
+const totalPopulation = document.querySelector(".total-population");
+const averagePopulation = document.querySelector(".average-population");
+const sum = arrayOfCountryPeople.map((pop)=>parsePopulation(pop.textContent)).reduce((acc, num)=>acc + num, 0);
+totalPopulation.textContent = sum;
+averagePopulation.textContent = sum / arrayOfCountryPeople.length;
+
+//# sourceMappingURL=index.f75de5e1.js.map
